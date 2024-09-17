23e0e6bb9a40094fd14a50baa1a937a51fbce21b
Aravinda Achar <141430809+aravindachar@users.noreply.github.com>
2024-09-17 21:31:29 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
-import java.util.Scanner;
+package Arrays;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        System.out.println("whats your name ?");
-        Scanner name = new Scanner(System.in);
+        // Array is collection of datatypes(premitive or nonpremitive)
+        // SYNTAXX
+        // datatype[] Variable_name = new datatype[size]; arrayobjects
+        // Direct 
+        // datatype[] Variable_name = {1,2,3,4,5,6,7};
 
-        System.out.println(name.nextFloat());   
+        int[] aravind;//declarition of array here 'aravind' id declared in the stack
+        aravind = new int[4]; //intialization of array 'aravind'
+        
+
     }
 }
