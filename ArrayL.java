021e68beed875cec6e5a65208fb890b654d95557
Aravinda Achar <141430809+aravindachar@users.noreply.github.com>
2024-09-26 21:00:17 +0530

    Add files via upload

---
+++ ArrayL.java
@@ -0,0 +1,31 @@
+package Arrays;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+public class ArrayL{
+    public static void main(String[] args) {
+        // input 
+        list.add(23);
+        list.add(23);
+        list.add(23);
+        list.add(23);
+        list.add(23);
+        Scanner in = new Scanner(System.in);
+        // Syntax
+        ArrayList<Interger> list = new ArrayList<Interger>();
+        // add 
+        for (int i = 0; i < args.length; i++) {
+            list.add(in.nextInt());
+        }
+
+        // Print
+        for (int i = 0; i < args.length; i++) {
+            System.out.println();
+        }
+        // Using Loop 
+        // Arraylist Functiins
+        list.remove(4);
+        list.set(2, 344);
+        
+    }   
+}
